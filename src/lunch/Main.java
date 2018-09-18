a4b133fff3e3bac5152463c6345a67d6a5737955
Komachka <katuxaz@ukr.net>
2018-09-19 00:44:13 +0300

    Validate for input args

--- src/lunch/Main.java
+++ src/lunch/Main.java
@@ -12,14 +12,25 @@ class Main{
 
     public static void main(String[] args){
 
+        final String FILENAME = "scenario.txt";
+        final String OUTPUTFILE_NAME = "simulation.txt";
+        
+        if(args.length != 1)
+        {
+            System.err.println("Invalid input");
+            System.exit(0);
+        }
+        if (!args[0].toLowerCase().equals(FILENAME)) {
+            System.err.println("Invalid input");
+            System.exit(0);
+        }
+
         for(int i = 0; i < args.length; i++)
         {
             System.out.println("i " + i+ " = " + args[i]);
         }
 
         int times;
-        String FILENAME = "scenario.txt";
-        final String OUTPUTFILE_NAME = "simulation.txt";
         BufferedReader br = null;
         Writter writter = null;
         String line;
